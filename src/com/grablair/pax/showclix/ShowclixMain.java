bd25a5433540b9f01170fbbd1024b358be1df052
Graham Blair <grablair@amazon.com>
2014-05-15 09:06:29 -0700

    First commit

---
+++ src/com/grablair/pax/showclix/ShowclixMain.java
@@ -0,0 +1,71 @@
+package com.grablair.pax.showclix;
+
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
+public class ShowclixMain {
+	public static final String SHOWCLIX_EVENTS_URL = "http://api.showclix.com/Seller/16886/events";
+	public static final long THRESHOLD_MILLIS = 5000;
+	public static final long WAIT_TIME_MILLIS = 1000;
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws IOException, InterruptedException {
+		URL url = new URL(SHOWCLIX_EVENTS_URL);
+		
+		Status status = new Status();
+		int latestId = 0;
+		long lastCheck = 0;
+		for (;;) {
+			try {
+				if (System.currentTimeMillis() - lastCheck >= THRESHOLD_MILLIS) {
+					HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+				    httpCon.addRequestProperty("User-Agent", "Mozilla/4.0");
+					BufferedReader reader = new BufferedReader(new InputStreamReader(httpCon.getInputStream()));
+					String jsonText = "";
+					while (reader.ready()) {
+						jsonText += reader.readLine();
+					}
+					reader.close();
+					
+					JSONObject obj = (JSONObject) JSONSerializer.toJSON(jsonText);
+					
+					int maxId = 0;
+					for (String s : (Iterable<String>) obj.keySet()) {
+						maxId = Math.max(maxId, Integer.parseInt((String) s));
+					}
+					
+					status.setTotalEventCount(obj.size());
+					
+					if (maxId != latestId) {
+						latestId = maxId;
+						JSONObject eventObj = (JSONObject) obj.get(Integer.toString(maxId));
+						status.setLatestEvent(eventObj.getString("event"));
+						String eventUrl = "http://showclix.com/event/" + maxId;
+						status.setLatestURL(eventUrl);
+						try {
+							Desktop.getDesktop().browse(new URI(eventUrl));
+						} catch (IOException | URISyntaxException e) {
+							e.printStackTrace();
+						}
+					}
+					
+					lastCheck = System.currentTimeMillis();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				status.setLatestShowclixCheck(System.currentTimeMillis() - lastCheck);
+				Thread.sleep(WAIT_TIME_MILLIS);
+			}
+		}
+	}
+}
---
+++ src/com/grablair/pax/showclix/Status.java
@@ -0,0 +1,95 @@
+package com.grablair.pax.showclix;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Status extends JFrame {
+
+	private static final long serialVersionUID = 8264954633550451627L;
+	
+	private JLabel title;
+	private JLabel latestShowclixTime;
+	private JLabel totalEventCount;
+	private JLabel latestEvent;
+	private JLabel latestEventURL;
+	
+	public Status() {
+		initComponents();
+		setVisible(true);
+	}
+	
+	public void setLatestShowclixCheck(long millis) {
+		latestShowclixTime.setText("Latest Showclix Check: " + (millis/1000) + " seconds ago");
+		
+		pack();
+	}
+	
+	public void setTotalEventCount(int count) {
+		totalEventCount.setText("Total Event Count: " + count);
+		
+		pack();
+	}
+	
+	public void setLatestURL(String url) {
+		latestEventURL.setText("Latest Event URL: " + url);
+		
+		pack();
+	}
+	
+	public void setLatestEvent(String url) {
+		latestEvent.setText("Latest Event: " + url);
+		
+		pack();
+	}
+	
+	public void initComponents() {
+		title = new JLabel();
+		latestShowclixTime = new JLabel();
+		totalEventCount = new JLabel();
+		latestEvent = new JLabel();
+		latestEventURL = new JLabel();
+		
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		
+		title.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+		title.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+		title.setText("PAX Showclix Status");
+		
+		latestShowclixTime.setText("Latest Showclix Check: ");
+		totalEventCount.setText("Total Event Count: ");
+		latestEvent.setText("Latest Event: ");
+		latestEventURL.setText("Latest Event URL: ");
+		
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+	    getContentPane().setLayout(layout);
+	    layout.setHorizontalGroup(
+	      layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	      .addGroup(layout.createSequentialGroup()
+	        .addContainerGap()
+	        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	          .addComponent(title)
+	          .addComponent(latestShowclixTime)
+	          .addComponent(totalEventCount)
+	          .addComponent(latestEvent)
+	          .addComponent(latestEventURL))
+	        .addContainerGap())
+	    );
+	    layout.setVerticalGroup(
+	      layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	      .addGroup(layout.createSequentialGroup()
+	        .addContainerGap()
+	        .addComponent(title)
+	        .addGap(18, 18, 18)
+	        .addComponent(latestShowclixTime)
+	        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+	        .addComponent(totalEventCount)
+	        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+	        .addComponent(latestEvent)
+	        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+	        .addComponent(latestEventURL)
+	        .addContainerGap())
+	    );
+
+	    pack();
+	}
+}
